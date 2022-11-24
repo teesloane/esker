4bbcc50dab110259971c34bea8883ea0694ae00b
teesloane <tyler.sloane@testdouble.com>
2022-11-24 16:58:15 -0500

    Start: Adding Clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Build your site
+    Build {
+        /// Directory of your site
+        #[arg(short, long, value_name = "DIR")]
+        dir: Option<PathBuf>
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Build { dir }) => {
+            if let Some(dir_path) = dir {
+                println!("Dir_path is {}", dir_path.display());
+            } else {
+                println!("Not printing testing lists...");
+            }
+        }
+        None => {}
+    }
 }
